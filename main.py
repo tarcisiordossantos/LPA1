cc2cafa06c5803b1a0fc6bec179c2c3809bbc90f
tarcisiordossantos <tarciisiorock@gmail.com>
2025-07-23 17:50:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 import pygame as pg
 
+print('Setup Start')
 pg.init()
-
 window = pg.display.set_mode(size=(600, 480))
+print('Setup End')
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
+            print('Quitting...')
             pg.quit()
             quit()
