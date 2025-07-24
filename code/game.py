7fb4257bdb81816d39e01cd800badd5fad3fd95e
tarcisiordossantos <tarciisiorock@gmail.com>
2025-07-24 19:19:26 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/game.py
+++ code/game.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import pygame as pg
 
-from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.level import Level
 from code.menu import Menu
 
 
@@ -14,5 +15,13 @@ class Game:
     def run(self, ):
         while True:
             menu = Menu(self.window)
-            menu.run()
+            menu_return = menu.run()
 
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level = Level(self.window, 'Level1', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[4]:
+                pg.quit()
+                quit()
+            else:
+                pass
--- code/menu.py
+++ code/menu.py
@@ -14,22 +14,43 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pg.mixer_music.load('./asset/Menu.mp3')
         pg.mixer_music.play(-1)
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
-
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_ORANGE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pg.display.flip()
 
+            #CHECK FOR ALL EVENTS
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     quit()
+
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pg.K_UP:
+                        if menu_option  > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pg.K_RETURN: #ENTER
+                        return MENU_OPTION[menu_option]
+
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         tex_font: Font = pg.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
