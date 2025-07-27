86b65ef515793bf84d4b9b865d56109848f8df41
tarcisiordossantos <tarciisiorock@gmail.com>
2025-07-26 21:50:15 -0300

    Level 2 implementation

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -12,11 +12,17 @@ class EntityFactory:
     @staticmethod
     def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
-            case 'Leve1Bg':
+            case 'Level1Bg':
                 list_bg = []
                 for i in range(7):
                     list_bg.append(Background(f'Level1Bg{i}', position))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):
+                    list_bg.append(Background(f'Level2Bg{i}', position))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
